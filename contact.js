208066f445103260e1200ed17d95d13ba8731e23
Atharva <atharvasharma.cs25@gmail.com>
2025-03-03 16:18:40 +0530

    [Atharva] Add. Added ability to store a contact detail in Address Book

---
+++ contact.js
@@ -0,0 +1,20 @@
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    toString() {
+        return `Name: ${this.firstName} ${this.lastName}, Address: ${this.address}, ${this.city}, ${this.state}, Zip: ${this.zip}, Phone: ${this.phone}, Email: ${this.email}`;
+    }
+}
+
+
+let contact1 = new Contact("John", "Doe", "123 Street", "NewYork", "NY", "123456", "9876543210", "john.doe@example.com");
+console.log(contact1.toString());
