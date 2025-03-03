1c389c2240c1485a9c1c318df0168d069584cab3
Atharva <atharvasharma.cs25@gmail.com>
2025-03-03 16:44:52 +0530

    [Atharva] Add. implemented functionality to prevent duplicate contact in Address Book

--- Main.js
+++ Main.js
@@ -9,16 +9,14 @@ try {
 
     let contact2 = new Contact("Jane", "Smith", "456 Avenue", "LosAngeles", "California", "789012", "8765432109", "jane.smith@example.com");
     myAddressBook.addContact(contact2);
+    // Trying to add a duplicate contact
+        let duplicateContact = new Contact("John", "Doe", "789 Boulevard", "San Francisco", "California", "654321", "9123456789", "john.duplicate@example.com");
+        myAddressBook.addContact(duplicateContact);
 } catch (error) {
     console.error(error.message);
 }
 
-console.log("Before deletion:");
+
 myAddressBook.displayContacts();
 myAddressBook.countContacts();
 
-myAddressBook.deleteContact("John", "Doe");
-
-console.log("After deletion:");
-myAddressBook.displayContacts();
-myAddressBook.countContacts();
